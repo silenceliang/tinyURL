b8cedb9490ebf3e6be0f4502f63f8d88644e1de9
silenceliang <l3754902@gmail.com>
2019-09-29 13:54:00 +0800

    add logging,parser,specifyURL

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import argparse
+import logging
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
 import string
 import redis
 from flask import Flask, render_template, redirect
@@ -14,15 +17,16 @@ class TinyURLRedis(object):
         except Exception as e:
             print(e)
 
-    def encode(self, longUrl):
+    def encode(self, longUrl, specify=None):
         def hash_units(dec, n=5):
             return abs(hash(dec)) % (10**n)
-        ServerPrefix = "http://localhost:5002/"
-        suffix = str(hash_units(longUrl))
+        if specify:
+            suffix = specify
+        else:
+            suffix = str(hash_units(longUrl))
         if self.r.get(suffix) is None:
             self.r.set(suffix, longUrl)
-
-        return ServerPrefix + suffix
+        return suffix
     
     def decode(self, shortUrl):
         idx = shortUrl.split('/')[-1]
@@ -45,7 +49,6 @@ class TinyURL(object):
                 dec //= 62
                 if not dec: break
             return result
-        ServerPrefix = "http://localhost:5002/"
         suffix  =decTo62(self.global_counter)
         if longUrl not in self.full_tiny:
             self.full_tiny[longUrl] = suffix
@@ -54,7 +57,7 @@ class TinyURL(object):
         else:
             suffix = self.full_tiny[longUrl]
 
-        return ServerPrefix + suffix
+        return suffix
 
     def decode(self, shortUrl):
         idx = shortUrl.split('/')[-1]
@@ -63,34 +66,42 @@ class TinyURL(object):
         else:
             return "Not exists such a url link"
 
-
 parser = argparse.ArgumentParser()
 parser.add_argument("--redis", default=True, action='store_true', help='using NoSQL-redis/hshTable for data saving.')
+parser.add_argument("--ip", default='localhost', help='server ip.')
+parser.add_argument("--port", default=5002, help='server port.')
 args = parser.parse_args()
-
-if args.redis:
-    fun = TinyURLRedis()  
-else:
-    fun = TinyURL()
+fun = TinyURLRedis() if args.redis else TinyURL()
 
 @app.route('/shortURL', methods=['POST'])
 def short_request():
+    ServerPrefix = 'http://' + args.ip + ':' + str(args.port) + '/' 
     url = request.get_json()
-    print('ready for encoding ...')
+    logging.info('ready for encoding ...')
     url_key = fun.encode(url)
-    print('after encoding: ', url_key)
-    return  {'key':url_key}
+    logging.info('after encoding:{}'.format(url_key))
+    return  {'key': ServerPrefix + url_key}
 
 @app.route('/<url_key>')
 def redir(url_key):
     url = fun.decode(url_key)
-    print(url)
+    logging.info('after decoding:{}'.format(url))
     if url is None: return False
     return redirect(url)
+
+@app.route('/specify/<specify_url>', methods=['POST'])
+def specify(specify_url):
+    ServerPrefix = 'http://' + args.ip + ':' + str(args.port) + '/' 
+    url = request.get_json()
+    logging.info('ready for encoding ...')
+    url_key = fun.encode(url, specify_url)
+    logging.info('after encoding:{}'.format(url_key))
+    return  {'key': ServerPrefix + url_key}
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
 if __name__=='__main__':
-    app.run(host='localhost', port=5002)
+    app.run(host=args.ip, port=args.port)
